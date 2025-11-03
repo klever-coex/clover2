262085856cb58cbb0a0377ccad6f17db0e3af5ed
l_motya <2663161@mail.ru>
2025-11-03 04:01:18 +0300

    fix: builder

--- tooling/builder/stages/01-common.sh
+++ tooling/builder/stages/01-common.sh
@@ -1,17 +1,15 @@
 
-stage() {
-    info "Upgrade system"
-    sudo apt update -qq
-    sudo apt upgrade -qqy
+info "Upgrade system"
+sudo apt update -qq
+sudo apt upgrade -qqy
 
-    info "Install common packages"
-    sudo apt install -qqy \
-        raspi-config \
-        meson \
-        cmake \
-        libboost-dev \
-        locales \
-        curl \
-        avahi-daemon \
-        libnss-mdns
-}
+info "Install common packages"
+sudo apt install -qqy \
+    raspi-config \
+    meson \
+    cmake \
+    libboost-dev \
+    locales \
+    curl \
+    avahi-daemon \
+    libnss-mdns
--- tooling/builder/stages/02-install-ros.sh
+++ tooling/builder/stages/02-install-ros.sh
@@ -1,24 +1,23 @@
-stage() {
-    info "Setup locales"
-    sudo apt update && sudo apt install locales
-    sudo locale-gen en_US en_US.UTF-8
-    sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
-    export LANG=en_US.UTF-8
 
-    info "Adding ROS2 repository"
-    sudo apt install -yq software-properties-common
-    sudo add-apt-repository -y universe
-    export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F\" '{print $4}')
-    curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo ${UBUNTU_CODENAME:-${VERSION_CODENAME}})_all.deb"
-    sudo dpkg -i /tmp/ros2-apt-source.deb
-    sudo apt update
-    sudo apt upgrade -yq
+info "Setup locales"
+sudo apt update && sudo apt install locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
 
-    info "Install ROS2"
-    sudo apt install -yq ros-dev-tools ros-$ROS_DISTRO-ros-base
+info "Adding ROS2 repository"
+sudo apt install -yq software-properties-common
+sudo add-apt-repository -y universe
+export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F\" '{print $4}')
+curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo ${UBUNTU_CODENAME:-${VERSION_CODENAME}})_all.deb"
+sudo dpkg -i /tmp/ros2-apt-source.deb
+sudo apt update
+sudo apt upgrade -yq
 
-    info "Install extra packages"
-    sudo apt install -yq geographiclib-tools
+info "Install ROS2"
+sudo apt install -yq ros-dev-tools ros-$ROS_DISTRO-ros-base
 
-    wget -qO- https://raw.githubusercontent.com/mavlink/mavros/master/mavros/scripts/install_geographiclib_datasets.sh | bash
-}
+info "Install extra packages"
+sudo apt install -yq geographiclib-tools
+
+wget -qO- https://raw.githubusercontent.com/mavlink/mavros/master/mavros/scripts/install_geographiclib_datasets.sh | sudo bash
