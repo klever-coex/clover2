e25a36eaec9474543d229d646a1f28a4b85f3f76
l_motya <2663161@mail.ru>
2025-11-04 04:55:14 +0300

    fix libcamera installation

--- tooling/builder/stages/02-ros.sh
+++ tooling/builder/stages/02-ros.sh
@@ -1,6 +1,6 @@
 
 log_info "Setup locales"
-sudo apt update && sudo apt install locales
+DEBIAN_FRONTEND=noninteractive sudo apt update && sudo apt install locales
 sudo locale-gen en_US en_US.UTF-8
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
