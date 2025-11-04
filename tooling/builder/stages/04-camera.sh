e404ffd5dd659be3efa31fcd003877400fd24886
l_motya <2663161@mail.ru>
2025-11-04 05:43:38 +0300

    fix stages

--- tooling/builder/stages/04-camera.sh
+++ tooling/builder/stages/04-camera.sh
@@ -1,6 +1,40 @@
 LIBCAMERA_VERSION="bfd68f786964636b09f8122e6c09c230367390e7"
 
 camera_libcamera() {
+    log_info "Install libcamera deps"
+    sudo apt install -y \
+        abi-compliance-checker \
+        clang \
+        cmake \
+        libcrypto++-dev \
+        libdw-dev \
+        libevent-dev \
+        libevent-dev \
+        libexif-dev \
+        libgstreamer-plugins-base1.0-dev \
+        libgstreamer1.0-dev \
+        libgtest-dev \
+        libjpeg-dev \
+        liblttng-ust-dev \
+        libpython3-dev pybind11-dev \
+        libssl-dev \
+        libtiff-dev \
+        libudev-dev \
+        libudev-dev \
+        libunwind-dev \
+        libyaml-dev \
+        libyuv-dev \
+        lttng-tools \
+        meson \
+        ninja-build \
+        openssl \
+        pkg-config \
+        pybind11-dev \
+        python3-jinja2 \
+        python3-jinja2 \
+        python3-ply \
+        python3-yaml
+
     log_info "Clone libcamera project (ver: $LIBCAMERA_VERSION)"
     LIBCAMERA_SOURCE_DIR=$(mktemp -d --suffix="-libcamera")
     git clone https://github.com/raspberrypi/libcamera.git $LIBCAMERA_SOURCE_DIR
@@ -9,7 +43,7 @@ camera_libcamera() {
 
     log_info "Build libcamera"
     meson setup build --buildtype=release -Dpipelines=rpi/vc4,rpi/pisp -Dipas=rpi/vc4,rpi/pisp -Dv4l2=enabled -Dgstreamer=enabled -Dtest=false -Dlc-compliance=disabled -Dcam=enabled -Dqcam=disabled -Ddocumentation=disabled -Dpycamera=enabled
-    ninja -C build install
+    yes | ninja -C build install
     sudo usermod -aG video $USER
 }
 
@@ -31,39 +65,9 @@ camera_ros_support() {
     sudo colcon build --install-base /opt/ros/$ROS_DISTRO/ --merge-install
 }
 
-log_info "Install libcamera deps"
-sudo apt install -y \
-    abi-compliance-checker \
-    clang \
-    cmake \
-    libcrypto++-dev \
-    libdw-dev \
-    libevent-dev \
-    libevent-dev \
-    libexif-dev \
-    libgstreamer-plugins-base1.0-dev \
-    libgstreamer1.0-dev \
-    libgtest-dev \
-    libjpeg-dev \
-    liblttng-ust-dev \
-    libpython3-dev pybind11-dev \
-    libssl-dev \
-    libtiff-dev \
-    libudev-dev \
-    libudev-dev \
-    libunwind-dev \
-    libyaml-dev \
-    libyuv-dev \
-    lttng-tools \
-    meson \
-    ninja-build \
-    openssl \
-    pkg-config \
-    pybind11-dev \
-    python3-jinja2 \
-    python3-jinja2 \
-    python3-ply \
-    python3-yaml
+if ! command -v cam >/dev/null 2>&1; then
+    log_info "Libcamera not installed"
+    camera_libcamera
+fi
 
-camera_libcamera
 camera_ros_support
