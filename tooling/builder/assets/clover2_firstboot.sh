63bcf9694e7c979b829f7e51eecdd396c8c35cfe
l_motya <2663161@mail.ru>
2025-12-09 01:22:22 +0300

    add netplan

--- tooling/builder/assets/clover2_firstboot.sh
+++ tooling/builder/assets/clover2_firstboot.sh
@@ -3,7 +3,7 @@
 echo "---> Fix home directory permissions <---"
 chmod +rx /home/pi
 
-NEW_SSID='clover2-'$(openssl rand -hex 2)
+NEW_SSID='clover2-'$(openssl rand -hex 3)
 NEW_HOSTNAME=$(echo ${NEW_SSID} | tr '[:upper:]' '[:lower:]')
 
 echo "---> Creating Wi-Fi AP with SSID=${NEW_SSID} <---"
@@ -12,12 +12,10 @@ nmcli con add type wifi ifname wlan0 mode ap con-name clover2 ssid $NEW_SSID aut
     && nmcli con modify clover2 ipv4.method shared ipv4.address 192.168.11.1/24 \
     && nmcli con modify clover2 ipv6.method disabled \
     && nmcli con modify clover2 wifi-sec.key-mgmt wpa-psk \
-    && nmcli con modify clover2 wifi-sec.psk "cloverwifi" \
-    && systemctl disable dnsmasq
+    && nmcli con modify clover2 wifi-sec.psk "cloverwifi"
 
-echo "---> Setting hostname to $NEW_HOSTNAME <---"
-hostnamectl set-hostname $NEW_HOSTNAME \
-    && sed -i 's/127\.0\.1\.1.*/127.0.1.1\t'${NEW_HOSTNAME}' '${NEW_HOSTNAME}'.local/g' /etc/hosts
+echo "---> Setting hostname to $NEW_SSID <---"
+hostnamectl set-hostname $NEW_SSID
 
 echo "---> Remove firstboot scrip  <---"
 systemctl disable clover2-firstboot.service
