ba3a2cf7a400faa67bd9a9a501fd02b6f903956e
l_motya <2663161@mail.ru>
2025-12-09 03:15:45 +0300

    fix netplan config name

--- tooling/builder/stages/07-netplan.sh
+++ tooling/builder/stages/07-netplan.sh
@@ -2,7 +2,7 @@ log_info "Install netplan config"
 
 sudo rm -rf /etc/netplan/50-cloud-init.yaml
 
-sudo cp $ASSETS_DIR/10-clover2.yaml /etc/netplan/
-sudo chmod 600 /etc/netplan/10-clover2.yaml
+sudo cp $ASSETS_DIR/10-rpi-netplan.yaml /etc/netplan/
+sudo chmod 600 /etc/netplan/10-rpi-netplan.yaml
 
 sudo netplan apply
