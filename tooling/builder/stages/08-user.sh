8826c8d4ed6d9230852e6c639eda1f5ff4deb940
l_motya <2663161@mail.ru>
2025-12-09 18:12:42 +0300

    install ohmyzsh to image

--- tooling/builder/stages/08-user.sh
+++ tooling/builder/stages/08-user.sh
@@ -6,6 +6,11 @@ sudo apt-get install -yq \
 
 sudo chsh -s $(which zsh)
 
+log_info "Install Oh My ZSH"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+echo "source /opt/ros/jazzy/setup.zsh" | tee -a /home/$USER/.zshrc
+echo "source /home/$USER/clover2_ws/install/setup.zsh" | tee -a /home/$USER/.zshrc
+
 log_info "Install ZSH settings"
 cp $ASSETS_DIR/zprofile /home/$USER/.zprofile
-cp $ASSETS_DIR/zshrc /home/$USER/.zshrc
