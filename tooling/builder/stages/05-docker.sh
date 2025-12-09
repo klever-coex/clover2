fa179ec051eb627625a318224188146ca2d162d5
l_motya <2663161@mail.ru>
2025-12-09 21:20:45 +0300

    fix docker logi

--- tooling/builder/stages/05-docker.sh
+++ tooling/builder/stages/05-docker.sh
@@ -10,4 +10,4 @@ log_info "Fix docker permissions"
 sudo usermod -aG docker $USER
 
 log_info "Login to registry"
-echo "$DOCKER_REGISTRY_PASSWORD" | sudo docker login $REGISTRY_HOST --username $DOCKER_REGISTRY_USER --password-stdin
+/bin/bash "echo $DOCKER_REGISTRY_PASSWORD | sudo docker login $REGISTRY_HOST --username $DOCKER_REGISTRY_USER --password-stdin"
