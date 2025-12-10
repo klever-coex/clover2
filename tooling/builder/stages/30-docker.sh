0d9504a447e8078d14e148c3e7216a6c307be5c8
l_motya <2663161@mail.ru>
2025-12-11 02:17:28 +0300

    remove docker login

--- tooling/builder/stages/30-docker.sh
+++ tooling/builder/stages/30-docker.sh
@@ -8,9 +8,3 @@ chmod +x /tmp/docker-install.sh
 
 log_info "Fix docker permissions"
 sudo usermod -aG docker $USER
-
-log_info "Login to registry"
-DOCKER_REGISTRY_USER=$(fetch_secret DOCKER_REGISTRY_USER)
-DOCKER_REGISTRY_PASSWORD=$(fetch_secret DOCKER_REGISTRY_PASSWORD)
-
-sudo docker login $REGISTRY_HOST -u $DOCKER_REGISTRY_USER -p $DOCKER_REGISTRY_PASSWORD
