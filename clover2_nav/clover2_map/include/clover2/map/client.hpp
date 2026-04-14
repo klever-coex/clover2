245c474ececd7cc48bea97613e06fb07c89e8203
l_motya <2663161@mail.ru>
2026-04-14 15:23:25 +0300

    fix callback group

--- clover2_nav/clover2_map/include/clover2/map/client.hpp
+++ clover2_nav/clover2_map/include/clover2/map/client.hpp
@@ -32,21 +32,19 @@ public:
         : m_logger(node->get_logger().get_child("map_client"))
         , m_map_valid(false)
         , m_name("") {
+        rclcpp::SubscriptionOptions options;
+        options.callback_group = cb_group;
+
         m_map_update_sub =
             node->template create_subscription<std_msgs::msg::Empty>(
                 "~/map_update", rclcpp::QoS(1).transient_local().reliable(),
                 std::bind(&client::map_update_callback, this,
-                          std::placeholders::_1));
+                          std::placeholders::_1),
+                options);
 
-        if (cb_group) {
-            m_get_map_client =
-                node->template create_client<clover2_pose_msgs::srv::GetMap>(
-                    "~/get_map", rclcpp::ServicesQoS(), cb_group);
-        } else {
-            m_get_map_client =
-                node->template create_client<clover2_pose_msgs::srv::GetMap>(
-                    "~/get_map", rclcpp::ServicesQoS());
-        }
+        m_get_map_client =
+            node->template create_client<clover2_pose_msgs::srv::GetMap>(
+                "~/get_map", rclcpp::ServicesQoS());
 
         update_map();
     }
