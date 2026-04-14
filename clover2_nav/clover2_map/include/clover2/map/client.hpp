8f769a4daa00bb81a88b102f03755ada57f18609
l_motya <2663161@mail.ru>
2026-04-14 15:35:03 +0300

    disable group

--- clover2_nav/clover2_map/include/clover2/map/client.hpp
+++ clover2_nav/clover2_map/include/clover2/map/client.hpp
@@ -32,15 +32,15 @@ public:
         : m_logger(node->get_logger().get_child("map_client"))
         , m_map_valid(false)
         , m_name("") {
-        rclcpp::SubscriptionOptions options;
-        options.callback_group = cb_group;
+        // TODO:
+        // rclcpp::SubscriptionOptions options;
+        // options.callback_group = cb_group;
 
         m_map_update_sub =
             node->template create_subscription<std_msgs::msg::Empty>(
                 "~/map_update", rclcpp::QoS(1).transient_local().reliable(),
                 std::bind(&client::map_update_callback, this,
-                          std::placeholders::_1),
-                options);
+                          std::placeholders::_1));
 
         m_get_map_client =
             node->template create_client<clover2_pose_msgs::srv::GetMap>(
