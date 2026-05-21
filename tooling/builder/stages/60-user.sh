837308a6fb33131713fedb3106db6ee05d1517a5
l_motya <2663161@mail.ru>
2026-05-21 23:15:58 +0300

    remove zsh install

--- tooling/builder/stages/60-user.sh
+++ tooling/builder/stages/60-user.sh
@@ -1,16 +1,7 @@
-log_info "Install ZSH"
+log_info "Install Shell Tools"
 sudo apt-get install -yq \
-    zsh \
     lolcat \
     figlet
-
-sudo chsh -s $(which zsh)
-
-log_info "Install Oh My ZSH"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-
-log_info "Install ZSH settings"
-cp $ASSETS_DIR/zshrc /home/$USER/.zshrc
 
 log_info "Install motd scripts"
 sudo rm -rf /etc/update-motd.d/*
