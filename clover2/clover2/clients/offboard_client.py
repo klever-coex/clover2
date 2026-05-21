1402228211c5d51440cf9c161d4162648e8bb454
l_motya <2663161@mail.ru>
2026-05-21 04:42:46 +0300

    sanitize python lib

--- clover2/clover2/clients/offboard_client.py
+++ clover2/clover2/clients/offboard_client.py
@@ -1,50 +1,17 @@
-import threading
-from types import NoneType
-from typing import Any
-
 from clover2_nav_msgs.action import NavigateAsync
 from clover2_nav_msgs.msg import State
 from clover2_nav_msgs.srv import ArmDisarm, Land, Navigate, SetPosition
 from geometry_msgs.msg import Pose
 from rclpy.action import ActionClient
 from rclpy.node import Node
-from rclpy.task import Future
 from tf_transformations import quaternion_from_euler
 
-from . import utils
+from ..utils import ActionHelper, ActionStatus, wait_future
 
 NAN = float("nan")
 
 
-class ActionHelper:
-    def __init__(self, action: ActionClient, goal):
-        self.event: threading.Event = threading.Event()
-        self.result: NoneType = None
-        self.message: str = "ok"
-        self.goal_future: Future = action.send_goal_async(goal)
-        self.goal_future.add_done_callback(self._response_callback)
-        self.get_result_future: Future | NoneType = None
-
-    def wait(self):
-        self.event.wait()
-
-    def _response_callback(self, future):
-        goal_handle = future.result()
-        if not goal_handle.accepted:
-            self.result = None
-            self.message = "Navigate goal reject"
-            self.event.set()
-            return
-
-        self.get_result_future = goal_handle.get_result_async()
-        self.get_result_future.add_done_callback(self._result_callback)
-
-    def _result_callback(self, future):
-        self.result = future.result().result
-        self.event.set()
-
-
-class Offboard:
+class OffboardClient:
     def __init__(self, node: Node):
         self._logger = node.get_logger().get_child("offboard")
 
@@ -80,7 +47,7 @@ class Offboard:
         req = ArmDisarm.Request()
         req.arm = arm
 
-        return self._wait_service_call(self._arm_disarm_client, req)
+        return self.__wait_service_call(self._arm_disarm_client, req)
 
     def arm(self) -> bool:
         return self.arm_disarm(True)
@@ -90,7 +57,7 @@ class Offboard:
 
     def land(self) -> bool:
         req = Land.Request()
-        return self._wait_service_call(self._land_client, req)
+        return self.__wait_service_call(self._land_client, req)
 
     def set_position(
         self,
@@ -104,9 +71,9 @@ class Offboard:
         req.header.frame_id = frame_id
         req.header.stamp = self._node.get_clock().now().to_msg()
 
-        req.pose = self._fill_pose(x, y, z, yaw)
+        req.pose = self.__fill_pose(x, y, z, yaw)
 
-        return self._wait_service_call(self._set_position_client, req)
+        return self.__wait_service_call(self._set_position_client, req)
 
     def navigate(
         self,
@@ -122,11 +89,11 @@ class Offboard:
         req.header.stamp = self._node.get_clock().now().to_msg()
 
         req.speed = speed
-        req.pose = self._fill_pose(x, y, z, yaw)
+        req.pose = self.__fill_pose(x, y, z, yaw)
 
-        return self._wait_service_call(self._navigate_client, req)
+        return self.__wait_service_call(self._navigate_client, req)
 
-    def navigate_async(
+    def navigate_wait(
         self,
         frame_id: str = "map",
         x: float = NAN,
@@ -134,30 +101,31 @@ class Offboard:
         z: float = NAN,
         yaw: float = NAN,
         speed: float = 0.5,
-    ):
+    ) -> bool:
         goal = NavigateAsync.Goal()
         goal.header.frame_id = frame_id
         goal.header.stamp = self._node.get_clock().now().to_msg()
 
         goal.speed = speed
-        goal.pose = self._fill_pose(x, y, z, yaw)
+        goal.pose = self.__fill_pose(x, y, z, yaw)
 
         self._navigate_async_aclient.wait_for_server()
 
         helper = ActionHelper(self._navigate_async_aclient, goal)
-        helper.wait()
+        status = helper.wait()
 
-        if helper.result is None:
-            self._node.get_logger().error(helper.message)
-            raise Exception(helper.message)
+        if status is ActionStatus.REJECTED:
+            raise RuntimeError(helper.message)
+
+        if status is not ActionStatus.SUCCEEDED:
+            raise RuntimeError(f"NavigateAsync {status.name}: {helper.message}")
 
         if not helper.result.success:
-            self._node.get_logger().error(helper.result.message)
-            raise Exception(helper.result.message)
+            raise RuntimeError(helper.result.message)
 
         return helper.result.success
 
-    def _fill_pose(self, x, y, z, yaw) -> Pose:
+    def __fill_pose(self, x, y, z, yaw) -> Pose:
         pose = Pose()
         pose.position.x = float(x)
         pose.position.y = float(y)
@@ -177,9 +145,9 @@ class Offboard:
     def _state_callback(self, msg: State):
         self._state = msg
 
-    def _wait_service_call(self, srv, request, timeout=1.0) -> bool:
+    def __wait_service_call(self, srv, request, timeout=1.0) -> bool:
         future = srv.call_async(request)
-        result = utils.wait_future(future, timeout=timeout)
+        result = wait_future(future, timeout=timeout)
 
         if not result:
             self._node.get_logger().error("Service not response")
--- clover2/clover2/clover2.py
+++ clover2/clover2/clover2.py
@@ -1,43 +1,53 @@
 import atexit
 import threading
+from types import NoneType
 
 import rclpy
 from rclpy.node import Node
 
 from . import utils
-from .offboard import Offboard
+from .clients import CameraClient, OffboardClient
 
 
 class Clover2(Node):
     def __init__(self, node_name: str = ""):
-        # Initialize ROS2 node
         if not node_name or len(node_name) == 0:
             node_name = "client_" + utils.generate_random_string(4)
-
-        self._ros_thread = None
-
-        self._init_ros(node_name)
-
-        self._offboard = Offboard(self)
-
-        # Stop function on exit
-        atexit.register(self._stop)
-
-    def _init_ros(self, node_name: str):
 
         rclpy.init()
         Node.__init__(self, node_name)
 
-        self._ros_thread = threading.Thread(target=self._ros_worker, daemon=True)
+        self._ros_thread: threading.Thread = threading.Thread(
+            target=self._ros_worker, daemon=True
+        )
         self._ros_thread.start()
+        _ = atexit.register(self._stop)
 
-        self.get_logger().info("ROS processor started")
+        self._offboard: OffboardClient = OffboardClient(self)
+        self._camera: CameraClient = CameraClient(self)
 
-    def _ros_worker(self):
+    def __getattr__(self, name: str):
+
+        for client in [self._offboard, self._camera]:
+            ret = getattr(client, name, None)
+            if ret:
+                return ret
+
+        raise RuntimeError(f"Unknown method {name}")
+
+    @property
+    def offboard(self) -> OffboardClient:
+        return self._offboard
+
+    @property
+    def camera(self) -> CameraClient:
+        return self._camera
+
+    def _ros_worker(self) -> None:
         while rclpy.ok():
             rclpy.spin(self)
 
-    def _stop(self):
+    def _stop(self) -> None:
         self.get_logger().debug(f"Exit callback for {self.get_name()}")
 
         if rclpy.ok():
@@ -47,23 +57,3 @@ class Clover2(Node):
                 self._ros_thread.join()
 
             self.destroy_node()
-
-    def is_armed(self) -> bool:
-        """Alias of is_armed()"""
-        return self._offboard.is_armed()
-
-    def arm(self) -> bool:
-        """Alias of arm()"""
-        return self._offboard.arm()
-
-    def disarm(self) -> bool:
-        """Alias of disarm()"""
-        return self._offboard.disarm()
-
-    def land(self) -> bool:
-        """Alias of land()"""
-        return self._offboard.land()
-
-    def flight_mode(self) -> str:
-        """Alias of get_mode()"""
-        return self._offboard.flight_mode()
