f2db40f61ca1f5192b41630044f196ad27d2f3c4
l_motya <2663161@mail.ru>
2026-05-22 00:19:32 +0300

    first try

--- tooling/builder/stages/60-user.sh
+++ tooling/builder/stages/60-user.sh
@@ -1,8 +1,3 @@
-log_info "Install Shell Tools"
-sudo apt-get install -yq \
-    lolcat \
-    figlet
-
 log_info "Install motd scripts"
 sudo rm -rf /etc/update-motd.d/*
 sudo cp $ASSETS_DIR/motd/* /etc/update-motd.d/
