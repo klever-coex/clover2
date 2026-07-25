367831dc431e648c3ae2507f7790d8d649a45513
matveylapin <2663161@mail.ru>
2026-07-25 10:07:01 +0300

    some save

--- tooling/scripts/version_manager.py
+++ tooling/scripts/version_manager.py
@@ -35,27 +35,69 @@ class Package:
 
     @property
     def version(self) -> semver.Version:
-        return self.__version.text
+        return semver.Version.parse(self.__version.text)
 
     @version.setter
     def version(self, value: str | semver.Version):
-        val = semver.Version.parse(value)
-        self.__version.text = str(val)
+        if isinstance(value, semver.Version):
+            self.__version.text = str(value)
+        else:
+            self.__version.text = str(semver.Version.parse(value))
 
 
-def build_package_list(base_path: pathlib.Path, filter: re.Pattern) -> list[Package]:
+def build_package_list(base_path: pathlib.Path, name_filter: re.Pattern) -> list[Package]:
+    packages = []
     for entry in base_path.rglob("package.xml"):
-        print(entry)
+        package = Package(entry)
+        logger.debug(f"Probe {package.path}")
+        if name_filter.search(package.name):
+            packages.append(package)
+    return packages
 
 
 def setup_logging(verbose: bool) -> None:
-    level = logging.DEBUG if verbose else logging.INFO
+    if verbose >= 3:
+        level = logging.DEBUG
+    elif verbose == 2:
+        level = logging.INFO
+    elif verbose == 1:
+        level = logging.WARNING
+    else:
+        level = logging.ERROR
 
     logging.basicConfig(
         level=level,
-        format="%(asctime)s [%(levelname)s] %(message)s",
+        format="[%(levelname)s] %(asctime)s: %(message)s",
         datefmt="%H:%M:%S",
     )
+
+
+def handle_print(args: argparse.Namespace, packages: list[Package]):
+    if len(packages) == 1:
+        print(packages[0].version)
+        return
+
+    for package in packages:
+        print(f"{package.name}: {package.version}")
+
+
+def handle_update(args: argparse.Namespace, packages: list[Package]):
+    new_version = args.new_version
+
+    for package in packages:
+        old = str(package.version)
+        package.version = new_version
+        package.save()
+        logger.info(f"Updated {package.name}: {old} - {package.version}")
+
+
+def handle_bump(args: argparse.Namespace, packages: list[Package]):
+    for package in packages:
+        old = str(package.version)
+        new = getattr(package.version, f"bump_{args.field}")()
+        package.version = new
+        package.save()
+        logger.info(f"Bumped {package.name}: {old} - {package.version}")
 
 
 def parse_args() -> argparse.Namespace:
@@ -63,8 +105,53 @@ def parse_args() -> argparse.Namespace:
         description="ROS2 package version manager"
     )
 
-    parser.add_argument("-f", "--filter", type=re.compile,
-                        help="Package name filter")
+    parser.add_argument(
+        "-d",
+        "--dir",
+        required=True,
+        type=pathlib.Path,
+        help="Dir for package search"
+    )
+    parser.add_argument(
+        "-f",
+        "--filter",
+        type=re.compile,
+        default=re.compile(".*"),
+        help="Package name filter"
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="count",
+        default=0,
+        help="Increase output verbosity"
+    )
+
+    subparsers = parser.add_subparsers(
+        dest="command",
+        help="Available subcommands",
+        required=True
+    )
+
+    parser_print = subparsers.add_parser("print", help="Print packages version")
+    parser_print.set_defaults(func=handle_print)
+
+    parser_update = subparsers.add_parser(
+        "update", help="Update packages version")
+    parser_update.add_argument(
+        "new_version",
+        help="New version",
+        type=str
+    )
+    parser_update.set_defaults(func=handle_update)
+
+    parser_bump = subparsers.add_parser("bump", help="Bump package version")
+    parser_bump.add_argument(
+        "field",
+        choices=["major", "minor", "patch"],
+        help="Version field to bump"
+    )
+    parser_bump.set_defaults(func=handle_bump)
 
     return parser.parse_args()
 
@@ -73,14 +160,10 @@ def main():
     args = parse_args()
     setup_logging(args.verbose)
 
+    packages = build_package_list(args.dir, args.filter)
+
+    args.func(args, packages)
+
 
 if __name__ == "__main__":
     main()
-
-    setup_logging(True)
-
-    build_package_list(pathlib.Path(
-        "/home/motya/own_projects/coex/clover2-dev/src"), "clover2*")
-
-    # val = Package(
-    #     "/home/motya/own_projects/coex/clover2-dev/src/clover2/clover2/package.xml")
