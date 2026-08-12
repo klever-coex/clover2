6063e902202336a7394173b8e7fdcf641319935e
matveylapin <2663161@mail.ru>
2026-08-12 22:13:45 +0300

    add cyclonedds config

--- tooling/builder/assets/ros2_launch.sh
+++ tooling/builder/assets/ros2_launch.sh
@@ -1,8 +1,6 @@
 #! /usr/bin/env bash
 
-export RCUTILS_COLORIZED_OUTPUT=1
-export ROS_AUTOMATIC_DISCOVERY_RANGE=LOCALHOST
-export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+. /etc/clover2/.ros2.env
 
 source /opt/ros/jazzy/setup.bash
 
